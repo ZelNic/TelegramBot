4f11e90304da519da4eae3fea5f3fd8c898a0f94
Nikolay <128342772+nikolayGameDevGO@users.noreply.github.com>
2023-06-20 17:33:01 +0500

    Initial commit

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("6062646845:AAGgh1fuVM8SBovfi1YYoX9vbv_uBnH77Tg");
+            botClient.StartReceiving(Update, Error);
+            Console.ReadKey();
+        }
+        private static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if(message != null) 
+            { 
+            
+            }
+
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            string errorMessage = $"Произошла ошибка: {exception.Message}";
+            errorMessage += $"\nStackTrace: {exception.StackTrace}";
+            return client.SendTextMessageAsync(chatId: "ваш ID чата", text: errorMessage, cancellationToken: token);
+        }
+
+    }   
+}
---
+++ TelegramBot/Recursion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    internal class Recursion
+    {
+        private static int total;
+
+        public static int Sum(List<int> numbers)
+        {
+            if (numbers.Count == 1)
+            {
+                total += numbers[0];
+                Console.WriteLine(total);
+                return total;
+            }
+            else
+            {
+                total += numbers[numbers.Count - 1];
+                if (numbers.Count != 1) numbers.RemoveAt(numbers.Count - 1);
+                Sum(numbers);
+                return total;
+            }
+        }
+    }
+}
